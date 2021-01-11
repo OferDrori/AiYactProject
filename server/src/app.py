95addbe271ef2b2fc72258b5cf8781036fd03ebb
Oferd <oferdrr100@gmail.com>
2021-01-11 14:30:58 +0200

    all files

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'hello test'
+@app.route('/')
+def hello_b():
+    return 'fadad'
 if __name__ == '__main__':
     app.run()
